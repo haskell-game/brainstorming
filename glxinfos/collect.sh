c88260c4f4b805e38d516ebcf155760abefda7e6
Brian Lewis <brian@lorf.org>
2013-07-07 15:55:14 -0500

    add first batch of contributed glxinfos

--- glxinfos/collect.sh
+++ glxinfos/collect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for filename in *.glxinfo
+for filename in {dev,contrib}/*.glxinfo
 do
   cat ${filename} | grep 'GL_' | tr ',' '\n' | grep -v '^ *$' | sed 's/ *//g' | sort -u
 done | sort | uniq -c | sed 's/^ *//' | sort -n -r
